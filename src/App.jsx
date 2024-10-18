e73aed764e113d5ea07835d32d2546931298f0c3
Beau Yancey <yancey.beau.j@gmail.com>
2024-10-17 18:52:48 -0600

    enhance(app) use react

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+	return (
+		<main>
+			<h1>Coming Soon :)</h1>
+			<p>
+				<a href="https://linkedin.com/in/hayleyyancey">Visit my LinkedIn</a>
+			</p>
+		</main>
+	)
+}
