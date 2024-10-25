b8b2499a889097a141234f736ceac3876cfdcadf
Beau Yancey <yancey.beau.j@gmail.com>
2024-10-25 14:33:49 -0600

    add(homepage)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home";
+import { Route, Routes } from "react-router-dom";
 
 export default function App() {
 	return (
 		<>
 			<Navbar />
-			{/* <Routes>
-				<Route path='/'/>
-			</Routes> */}
+			<Routes>
+				<Route path='/' element={<Home />}/>
+			</Routes>
 		</>
 	)
 }
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,20 @@
+import './Home.css'
+
+export default function Home() {
+	return (
+		<main className='home'>
+			<h1>Hi! I'm Hayley.</h1>
+			<img className='headshot' alt='Hayley Nickels Yancey' src='https://media.licdn.com/dms/image/v2/D5603AQGgsyolLS5XIQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1728870866806?e=1735171200&v=beta&t=KfEzlB8PgkDHl_uBptvMYLAr9Kqh0LNDKHrnytprZE0'/>
+			<div className='bio'>
+				<p>I'm glad you're here ˙ᵕ˙</p>
+				<p>I’m an instructional designer who loves utilizing technology to make learning engaging and accessible.</p>
+			</div>
+
+			<div className='projects-banner'>
+				<p>Projects ↓ ↓ ↓</p>
+			</div>
+
+			<section id='projects'></section>
+		</main>
+	)
+}
