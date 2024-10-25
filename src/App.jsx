d60bd69f577d5a670a0820801224e1ce0a2f4854
Beau Yancey <yancey.beau.j@gmail.com>
2024-10-25 15:27:29 -0600

    enhance(routing) to include coming soon and not found pages

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,31 @@ export default function App() {
 		<>
 			<Navbar />
 			<Routes>
-				<Route path='/' element={<Home />}/>
+				<Route path='/' exact element={<Home />}/>
+				<Route path='/about' element={<ComingSoon />} />
+				<Route path='/work' element={<ComingSoon />} />
+				<Route path='/resources' element={<ComingSoon />} />
+				<Route path='*' element={<NotFound />}/>
 			</Routes>
 			<Footer />
 		</>
+	)
+}
+
+function ComingSoon() {
+	return (
+		<main className='not-found'>
+			<h1>Coming Soon ˙ᵕ˙</h1>
+			<p>Stay tuned for updates to this page</p>
+		</main>
+	)
+}
+
+function NotFound() {
+	return (
+		<main className='not-found'>
+			<h1>Page Not Found</h1>
+			<p>Try using the links at the top of the page to get around ˙ᵕ˙</p>
+		</main>
 	)
 }
