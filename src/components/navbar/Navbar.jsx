46dd730baaab0d8c5a9a06f58406b90d72331206
Beau Yancey <yancey.beau.j@gmail.com>
2024-10-25 19:02:19 -0600

    enhance(navbar) show active tab

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,21 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Navbar.css';
 
 export default function Navbar() {
+
+	const location = useLocation()
+	const locationIncludes = (path) => {
+		return location.pathname.includes(path) ?
+			'active' : ''
+	}
+
 	return (
 		<nav>
 			<Link className='nav-name' to='/'>Hayley Nickels Yancey</Link>
 			<div className='nav-links'>
-				<Link to='/about'>About</Link>
-				<Link to='/work'>Work</Link>
-				<Link to='/resources'>Open Resources</Link>
+				<Link className={locationIncludes('/about')} to='/about'>About</Link>
+				<Link className={locationIncludes('/work')} to='/work'>Work</Link>
+				<Link className={locationIncludes('/resources')} to='/resources'>Open Resources</Link>
 			</div>
 		</nav>
 	)
